9322b68181e253719d0cc7da3571f9bc902c80e5
sony <sony.sth8@gmail.com>
2024-05-31 00:42:53 +0200

    Import abox automated

--- repository_creation.py
+++ repository_creation.py
@@ -53,8 +53,21 @@ config_ttl_template = """
   rdfs:label "" .
 """
 
+# Function to delete an existing repository in GraphDB
+def delete_repository(repo_id):
+    url = f'{GRAPHDB_BASE_URL}/rest/repositories/{repo_id}'
+    response = requests.delete(url)
+    print(f"Delete repository response status: {response.status_code}")
+    if response.status_code == 204:
+        print(f"Repository '{repo_id}' deleted successfully.")
+    else:
+        print(f"Failed to delete repository '{repo_id}': {response.text}")
+
 # Function to create a repository in GraphDB
 def create_repository(repo_id):
+    # Check if repository exists and delete if it does
+    delete_repository(repo_id)
+    
     # Save the configuration to a file
     config_ttl = config_ttl_template.format(repo_id, repo_id)
     config_file_path = f'{repo_id}_config.ttl'
@@ -77,10 +90,11 @@ def create_repository(repo_id):
 
 # Function to upload RDF data to a repository
 def upload_data_to_repository(repo_id, data, graph_uri, create_metadata_entry=True):
+    encoded_data = data.encode('utf-8')
     response = requests.post(
         f'{GRAPHDB_BASE_URL}/repositories/{repo_id}/statements',
         params={'context': f'<{graph_uri}>'},
-        data=data,
+        data=encoded_data,
         headers={'Content-Type': 'application/x-turtle'}
     )
     
@@ -117,7 +131,7 @@ def create_metadata(repo_id, action, graph_uri):
     upload_data_to_repository('metadata', metadata_graph.serialize(format='turtle'), graph_uri, create_metadata_entry=False)
 
 # Function to create and load TBox for local datasets
-def create_and_load_local_datasets(repo_id, tbox_data, graph_uri):
+def create_and_load_local_datasets(repo_id, tbox_data, abox_data, graph_uri):
     # Create repository
     create_repository(repo_id)
 
@@ -125,8 +139,13 @@ def create_and_load_local_datasets(repo_id, tbox_data, graph_uri):
     tbox_graph = Graph()
     tbox_graph.parse(data=tbox_data, format="ttl")
 
+    # Create TBox
+    abox_graph = Graph()
+    abox_graph.parse(data=abox_data, format="ttl")
+
     # Upload TBox to repository
     upload_data_to_repository(repo_id, tbox_graph.serialize(format='turtle'), graph_uri)
+    upload_data_to_repository(repo_id, abox_graph.serialize(format='turtle'), graph_uri)
 
 # Create metadata repository first
 create_repository('metadata')
@@ -137,19 +156,36 @@ tbox_files = {
     'customer': './output/tbox/tbox_customer.ttl',
     'customer_inventory': './output/tbox/tbox_customer_inventory.ttl',
     'customer_location': './output/tbox/tbox_customer_location.ttl',
-    'location': './/output/tbox/tbox_location.ttl',
+    'location': './output/tbox/tbox_location.ttl',
     'supermarkets': './output/tbox/tbox_supermarket.ttl',
     'spicybytes_b2c': './output/tbox/tbox_spicybytes_b2c.ttl',
     'spicybytes_c2c': './output/tbox/tbox_spicybytes_c2c.ttl'
 }
 
+# File paths for Abox data
+abox_files = {
+    'business_inventory': './output/abox/abox_business_inventory.ttl',
+    'customer': './output/abox/abox_customer.ttl',
+    'customer_inventory': './output/abox/abox_customer_inventory.ttl',
+    'customer_location': './output/abox/abox_customer_location.ttl',
+    'location': './output/abox/abox_location.ttl',
+    'supermarkets': './output/abox/abox_supermarket.ttl',
+    'spicybytes_b2c': './output/abox/abox_spicybytes_b2c.ttl',
+    'spicybytes_c2c': './output/abox/abox_spicybytes_c2c.ttl'
+}
+
+
 # Function to load data from files
-def load_data_from_file(file_path):
-    with open(file_path, 'r') as file:
+def load_data_from_file(file_path, encoding='utf-8'):
+    with open(file_path, 'r', encoding=encoding) as file:
         return file.read()
+    
 
 # Load local datasets
 for repo_id in tbox_files:
     tbox_data = load_data_from_file(tbox_files[repo_id])
+    abox_data = load_data_from_file(abox_files[repo_id])
     graph_uri = f'http://www.example.edu/spicy_bytes/'
-    create_and_load_local_datasets(repo_id, tbox_data, graph_uri)
+    create_and_load_local_datasets(repo_id, tbox_data, abox_data, graph_uri)
+
+
